66abbdfa34b1a15bed18676038b550f2da4d7b12
enochwu <enochwu7@gmail.com>
2017-12-07 18:36:31 -0800

    JS update
    Questions added as objects to array.

---
+++ assets/js/app.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+
+var questions = [
+  q1 = {
+    test: "This is question 1",
+    question: "What's Bart's classic phrase?",
+    a1: "D'oh!",
+    a2: "Take a hike!",
+    a3: "Eat my shorts!",
+    a4: "Get outta here!",
+  },
+
+  q2 = {
+    test: "This is question 2",
+    question: "What year did \"The Simpsons\" debut on television?",
+    a1: "1989",
+    a2: "1995",
+    a3: "1988",
+    a4: "1992",
+  },
+
+  q3 = {
+    test: "This is question 3",
+    question: "What aspect of the show's opening sequence NEVER changes?",
+    a1: "Bart writing on the school chalkboard.",
+    a2: "Maggie's actions in the checkout line.",
+    a3: "Lisa's saxophone solos.",
+    a4: "The family's entrance into the living room.",
+  },
+
+  q4 = {
+    test: "This is question 4",
+    question: "How many U.S. cities bear the name Springfield?",
+    a1: "27",
+    a2: "41",
+    a3: "15",
+    a4: "22",
+  },
+
+]
+
+console.log(q1.question);
+console.log(q2.question);
+console.log(q3.question);
+console.log(q4.question);
+console.log(questions);
+console.log(questions.length)
+
+})
